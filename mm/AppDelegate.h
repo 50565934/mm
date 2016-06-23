062bf83776d2535a77faa08ce5dc3002c62bcaf6
50565934 <MAC@MACdeiMac-3.local>
2016-06-23 17:35:01 +0800

    x
    x

---
+++ mm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mm
+//
+//  Created by MAC on 16/6/23.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
